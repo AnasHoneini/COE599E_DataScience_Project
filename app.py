08071961269b114d374e5a4c38d23d3af1d244dc
Anas Honeini <anas.hneineh@gmail.com>
2023-05-14 19:22:30 +0300

    image uploading

--- app.py
+++ app.py
@@ -6,18 +6,19 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-
-@app.route('/home')
 def home():
+    name = ["anas","honeini"]
+
     if request.method == "POST":
         # get the uploaded image file
-
-        return "Image uploaded successfully!"
+        file = request.files['image']
+        print(file)
+        return "File uploaded successfully"
+        #proccess it in google collab
+        # put the details in a list called name
         
-    return render_template("main.html")
+    return render_template("main.html",name=name)
 
 
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
